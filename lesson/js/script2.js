7e31694c90b7c3f271d31f245aadc4bb0de94d1d
gina79129 <lovemethanks@gmail.com>
2021-12-08 10:11:51 +0800

    add for html function

--- lesson/js/script2.js
+++ lesson/js/script2.js
@@ -1,3 +1,10 @@
+/**
+ *  html()        修改HTML內容 取得HTML內容
+    prepend()     插入元素 移動元素(最前面)
+    append()      插入元素 移動元素(最後面)
+    remove()      刪除元素
+    index()       取得順序編號
+ */
 $(function(){
     $("button").click(function(){
         //修改HTML內容 html() 將HTML原始碼當作參數傳入，即可修改元素內容
@@ -14,5 +21,12 @@ $(function(){
         // $("#append").append("<li>a3</li>");
         //移動元素 append() 將可用來表示元素順序的虛擬類別當做參數傳入時，則可將該元素移到選擇器所指元素內的最後面
         $("#moapp").append($("li:first-child"));
+
+        //刪除元素 remove() 刪除指定元素
+        $("#rmd").remove();
+
+        //取得順序編號 index() 取得選擇器所指向元素在同一階層元素中的順序編號。編號由0開始起跳
+        //只要是屬性於同一階層的元素，就算是元素類型不同，一樣會被計入
+        $("#moapp").html($("#showindex").index());
     })
 })
